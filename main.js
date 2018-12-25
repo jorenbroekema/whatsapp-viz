d9671dd3e1dd394e684abd8f9bf579563b89e2ff
Cornelis de Mooij <codemo@gmail.com>
2018-12-25 13:01:38 +0100

    Changed the plot to a treemap instead of doughnut, more readable. Colors are now biased around YC Orange

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ fetch('./whatsapp-data/example.json').then((response) => {
   return response.json();
 }).then((json) => {
   const dataByUser = sortMessagesByUser(json.data);
-  renderDoughtnut(dataByUser);
+  //renderDoughnut(dataByUser);
+  renderTreemap(dataByUser);
 });
 
 const sortMessagesByUser = data => {
@@ -18,7 +19,7 @@ const sortMessagesByUser = data => {
   return dataByUser;
 };
 
-const renderDoughtnut = (data) => {
+const renderDoughnut = (data) => {
   let doughnut = {
     type: 'doughnut',
     data: {
@@ -55,5 +56,62 @@ const renderDoughtnut = (data) => {
   var ctx = document.getElementById("myChart").getContext("2d");
   new Chart(ctx, {
     ...doughnut
+  });
+}
+
+const renderTreemap = (data) => {
+  // Put the data into the right form for a treemap:
+  let series = [];
+  Object.keys(data).forEach(function(key) {
+    series.push(
+      {
+        "text": key + " " + data[key].length,
+        "value": data[key].length
+      }
+    )
+  })
+
+  // Set some random (but distributed) colors:
+  let nUsers = Object.keys(data).length;
+  let n = Math.ceil(Math.pow(nUsers, 0.333333));
+  let palette = [];
+  Object.keys(data).forEach((user, index) => {
+    function componentToHex(c) {
+        var hex = c.toString(16);
+        return hex.length == 1 ? '0' + hex : hex;
+    }
+
+    let rBias = 255; let gBias = 114; let bBias = 42; // Bias the colors around YC Orange.
+
+    let r = Math.floor((((index / (n*n) % n)) * (255 - rBias)) / n) + rBias;
+    let g = Math.floor((((index / n) % n) * (255 - gBias)) / n) + gBias;
+    let b = Math.floor(((index % n) * (255 - bBias)) / n) + bBias;
+
+    let hex = "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+    console.log(hex);
+    palette.push(hex);
+  })
+
+  // Define the chart:
+  let chartData = {
+    plotarea: {
+       margin:"40 0 0 0"
+    },
+    "options":{
+      "aspect-type":"palette",
+      "palette":palette
+    },
+    type: 'treemap',
+    title: {
+      text: 'Treemap of People & Message Count'
+    },
+    series: series
+  };
+
+  // Render:
+  zingchart.render({
+    id: 'chartDiv',
+    data: chartData,
+    height: 950,
   });
 }
